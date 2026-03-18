921279b6ee81d820e7aafb5f611ab264fbd9a90b
lohit-bruno <lohit@usebruno.com>
2026-03-18 14:01:23 +0530

    updates

--- tests/proxy/badssl_client_cert_download.ts
+++ tests/proxy/badssl_client_cert_download.ts
@@ -4,10 +4,8 @@ import * as path from 'path';
 import * as https from 'https';
 
 const BADSSL_P12_URL = 'https://badssl.com/certs/badssl.com-client.p12';
-const BADSSL_PEM_URL = 'https://badssl.com/certs/badssl.com-client.pem';
 const CERT_DIR = path.join(__dirname, 'server', 'certs');
 const P12_PATH = path.join(CERT_DIR, 'badssl-client.p12');
-const PEM_PATH = path.join(CERT_DIR, 'badssl-client.pem');
 
 function download(url: string, dest: string): Promise<void> {
   return new Promise((resolve, reject) => {
@@ -42,13 +40,12 @@ function download(url: string, dest: string): Promise<void> {
 test('download badssl client certificate', async () => {
   await fs.promises.mkdir(CERT_DIR, { recursive: true });
 
-  if (fs.existsSync(PEM_PATH) && (await fs.promises.stat(PEM_PATH)).size > 0) {
-    console.log(`badssl client cert already exists at ${CERT_DIR}`);
+  if (fs.existsSync(P12_PATH) && (await fs.promises.stat(P12_PATH)).size > 0) {
+    console.log(`badssl client cert already exists at ${P12_PATH}`);
     return;
   }
 
   console.log('Downloading badssl client certificate...');
   await download(BADSSL_P12_URL, P12_PATH);
-  await download(BADSSL_PEM_URL, PEM_PATH);
-  console.log(`Downloaded to ${CERT_DIR}`);
+  console.log(`Downloaded to ${P12_PATH}`);
 });
